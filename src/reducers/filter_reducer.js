bb9e3438b027e6f6461275354b6781d28973336d
AydanKusland <chinatugov@mail.ru>
2023-05-09 17:43:00 +0800

    41/87

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,17 +1,56 @@
 import {
-  LOAD_PRODUCTS,
-  SET_LISTVIEW,
-  SET_GRIDVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
+	LOAD_PRODUCTS,
+	SET_LISTVIEW,
+	SET_GRIDVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS
 } from '../actions'
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
+	if (action.type === LOAD_PRODUCTS) {
+		let maxPrice = action.payload.map(p => p.price)
+		maxPrice = Math.max(...maxPrice)
+		return {
+			...state,
+			all_products: [...action.payload],
+			filtered_products: [...action.payload],
+			filters: { ...state.filters, max_price: maxPrice, price: maxPrice }
+		}
+	}
+	if (action.type === SET_LISTVIEW) {
+		return { ...state, grid_view: false }
+	}
+	if (action.type === SET_GRIDVIEW) {
+		return { ...state, grid_view: true }
+	}
+	if (action.type === UPDATE_SORT) {
+		return { ...state, sort: action.payload }
+	}
+	if (action.type === SORT_PRODUCTS) {
+		const { sort, filtered_products } = state
+		let tempProducts = [...filtered_products]
+		if (sort === 'price-lowest') {
+			tempProducts.sort((a, b) => a.price - b.price)
+		}
+		if (sort === 'price-highest') {
+			tempProducts.sort((a, b) => b.price - a.price)
+		}
+		if (sort === 'name-a') {
+			tempProducts.sort((a, b) => a.name.localeCompare(b.name))
+		}
+		if (sort === 'name-z') {
+			tempProducts.sort((a, b) => b.name.localeCompare(a.name))
+		}
+		if (action.type === UPDATE_FILTERS) {
+			return { ...state, sort: action.payload }
+		}
+		return { ...state, filtered_products: tempProducts }
+	}
+	return state
+	throw new Error(`No Matching "${action.type}" - action type`)
 }
 
 export default filter_reducer
