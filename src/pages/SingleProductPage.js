8c923e27e3d65c7d57d4f747ed38ccef3a017670
AydanKusland <chinatugov@mail.ru>
2023-05-09 09:31:05 +0800

    32/87

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -58,11 +58,11 @@ const SingleProductPage = () => {
 				<Link to='/products' className='btn'>
 					Back to products
 				</Link>
-				<div className='products-center'>
-					<ProductImages />
+				<div className='product-center'>
+					<ProductImages images={images} />
 					<section className='content'>
 						<h2>{name}</h2>
-						<Stars />
+						<Stars stars={stars} reviews={reviews} />
 						<h5 className='price'>{formatPrice(price)}</h5>
 						<p className='description'>{description}</p>
 						<p className='info'>
@@ -78,6 +78,7 @@ const SingleProductPage = () => {
 							{company}
 						</p>
 						<hr />
+						{stock > 0 && <AddToCart product={product} />}
 					</section>
 				</div>
 			</div>
