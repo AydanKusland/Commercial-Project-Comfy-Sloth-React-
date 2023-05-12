aa1d80bbae992fc1dea046d81e44db452c55b072
AydanKusland <chinatugov@mail.ru>
2023-05-12 18:46:29 +0800

    84/87

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
 	<Auth0Provider
-		domain='dev-xscmrghxtb3684k3.us.auth0.com'
-		clientId='ucohjbLQhBJXuNQ2OSB9w3jiLvNWUVem'
+		domain={process.env.REACT_APP_AUTH_DOMAIN}
+		clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
 		authorizationParams={{
 			redirect_uri: window.location.origin
 		}}
