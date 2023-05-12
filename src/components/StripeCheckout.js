4fcc3fa4258a7c75c491073b0eb45996bc379f06
AydanKusland <chinatugov@mail.ru>
2023-05-12 23:12:31 +0800

    finished

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -54,9 +54,10 @@ const CheckoutForm = () => {
 				'./.netlify/functions/create-payment-intent',
 				JSON.stringify({ cart, shipping_fee, total_amount })
 			)
-			console.log(data)
+			setClientSecret(data.clientSecret)
+			console.log(data.clientSecret)
 		} catch (error) {
-			console.log(error)
+			// console.log(error.response)
 		}
 	}
 
@@ -65,11 +66,47 @@ const CheckoutForm = () => {
 		// eslint-disable-next-line
 	}, [])
 
-	const handleChange = async ev => {}
-	const handleSubmit = async ev => {}
+	const handleChange = async event => {
+		setDisabled(event.empty)
+		setError(event.error ? event.error.message : '')
+	}
+	const handleSubmit = async ev => {
+		ev.preventDefault()
+		setProcessing(true)
+		const payload = await stripe.confirmCardPayment(clientSecret, {
+			payment_method: {
+				card: elements.getElement(CardElement)
+			}
+		})
+		if (payload.error) {
+			setError(`Payment failed ${payload.error.message}`)
+			setProcessing(false)
+		} else {
+			setError(null)
+			setProcessing(false)
+			setSucceeded(true)
+			setTimeout(() => {
+				clearCart()
+				navigate('/')
+			}, 5000)
+		}
+	}
 
 	return (
 		<div>
+			{succeeded ? (
+				<article>
+					<h4>Thank you</h4>
+					<h4>Your payment was successful</h4>
+					<h4>Redirecting to home page shortly</h4>
+				</article>
+			) : (
+				<article>
+					<h4>Hello, {myUser && myUser.name}</h4>
+					<p>Your total is {formatPrice(shipping_fee + total_amount)}</p>
+					<p>Test Card Number : 4242 4242 4242 4242</p>
+				</article>
+			)}
 			<form id='payment-form' onSubmit={handleSubmit}>
 				<CardElement
 					id='card-element'
@@ -91,7 +128,7 @@ const CheckoutForm = () => {
 				<p className={succeeded ? 'result-message' : 'result-message hidden'}>
 					Payment succeeded, see the result in your
 					<a href={`https://dashboard.stripe.com/test/payments`}>
-						Stripe dashboard.
+						&nbsp;Stripe dashboard.&nbsp;
 					</a>
 					Refresh the page to pay again
 				</p>
